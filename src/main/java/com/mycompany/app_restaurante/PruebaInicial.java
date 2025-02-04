53d28f5f69fcb3eba3ca1d77572e3d4d1e11ee16
Jhonatan Oro <jhonatan_oro@usmp.pe>
2025-02-04 00:02:19 -0500

    Prueba Inicial

---
+++ src/main/java/com/mycompany/app_restaurante/PruebaInicial.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.app_restaurante;
+
+/**
+ *
+ * @author jhowi
+ */
+public class PruebaInicial {
+    
+   public String PruebaReal;
+    
+}
